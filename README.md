a8a3c86e18caabec3f2b1be8cea3684a27fa61f0
Phaedrus <phraznikov@gmail.com>
2018-11-20 18:28:32 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scratch
+
+Disposable notes from the command line.
+
+## Usage
+`scratch` opens up a markdown file in your home directory with *vim*.
+
+## LICENSE
+[MIT](https://opensource.org/licenses/MIT)
---
+++ scratch.go
@@ -0,0 +1,51 @@
+package main
+
+// scratch
+// Disposable command line notes
+
+import (
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func scratchpath() string {
+	usr, err := user.Current()
+	check(err)
+	return filepath.Join(usr.HomeDir, "scratchpad.md")
+}
+
+func makePad(p string) {
+	f, err := os.Create(p)
+	check(err)
+	defer f.Close()
+	_, err = f.WriteString("# Scratchpad\n\n\n")
+	f.Sync()
+	check(err)
+}
+
+func openPad(p string) {
+	cmd := exec.Command("vim", p)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	check(err)
+}
+
+func scratch() {
+	p := scratchpath()
+	makePad(p)
+	openPad(p)
+}
+
+func main() {
+	scratch()
+}
